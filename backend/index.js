61c03c4032300c1b292c628e169beda1b0113e20
Aakash Garg <aakashgarg1009@gmail.com>
2021-11-13 18:20:54 +0530

    implemented bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
-const connectToMongo = require("./db");
-const express = require("express");
-connectToMongo();
-const app = express();
-const port = 3000;
+const connectToMongo = require('./db');
+const express = require('express')
 
-// Available routes
-app.use("/api/auth", require("./routes/auth"));
-// app.use('/api/notes',require("./routes/notes"))
+connectToMongo();
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
