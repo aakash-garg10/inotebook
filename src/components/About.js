d44cc87dfeaf16675b373fd5d12b4480e88f40c1
Aakash Garg <aakashgarg1009@gmail.com>
2021-11-14 18:35:29 +0530

    Fetching Notes and noteItem added

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,17 @@ import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
-    // console.log(a.state.name)
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
-    return (
-        <div>
-            This is About {a.state.name} and he is in class {a.state.class}
-        </div>
-    )
+    // const a = useContext(noteContext)
+    // // console.log(a.state.name)
+    // useEffect(() => {
+    //     a.update();
+    //     // eslint-disable-next-line
+    // }, [])
+    // return (
+    //     <div>
+    //         {/* This is About {a.state.name} and he is in class {a.state.class} */}
+    //     </div>
+    // )
 }
 
 export default About
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,27 +1,49 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 
-const NoteState = (props)=>{
-    const s1 = {
-        "name": "Harry",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
+const NoteState = (props) => {
+  // THIS IS JUST TO GET HOLD OF CONTEXTAPI
+  // const s1 = {
+  //     "name": "Harry",
+  //     "class": "5b"
+  // }
+  // const [state, setState] = useState(s1);
 
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": "Larry",
-                "class": "10b"
-            })
-        }, 2500);
-    }
-    return (
-        // this provider is used to give access value to all the elements wtapped inside the NoteState 
-        <NoteContext.Provider value={{state:state, update:update}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
-}
+  // const update = ()=>{
+  //     setTimeout(() => {
+  //         setState({
+  //             "name": "Larry",
+  //             "class": "10b"
+  //         })
+  //     }, 2500);
+  // }
+  const notesInitial = [
+    {
+      _id: "618fc08da7f1830ce23cb1cb",
+      user: "618fb27708efde20d7aa12dd",
+      title: "First Note",
+      description: "this is the first note of the project",
+      tag: "General",
+      date: "2021-11-13T13:41:33.029Z",
+      __v: 0,
+    },
+    {
+      _id: "61909f54ddcd7b477d4700b1",
+      user: "618fb27708efde20d7aa12dd",
+      title: "First Note",
+      description: "this is the second note note of the project",
+      tag: "General",
+      date: "2021-11-14T05:32:04.636Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setNotes] = useState(notesInitial);
+
+  return (
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
 
 export default NoteState;
