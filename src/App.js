921fb4a02b04f702dae14eef63fec8d1370e5426
Aakash Garg <aakashgarg1009@gmail.com>
2021-11-15 20:30:24 +0530

    login ADDed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Home } from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
 import { Alert } from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message="this is noicceeeeee"/>
+          <Alert message="this is noicceeeeee" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/Signup" element={<Signup />} />
             </Routes>
           </div>
         </Router>
